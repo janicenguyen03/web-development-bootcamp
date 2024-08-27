30997dea1d9d3fece35753b8a23351e4e94446da
Janice Nguyen <haidn@uci.edu>
2024-08-27 15:07:10 +0700

    express and postman

---
+++ Backend/Node/2.3 NPM/index.js
@@ -0,0 +1,11 @@
+// var generateName = require("sillyName");
+// import generateName from "sillyName";
+
+import {randomSuperhero} from "superheroes";
+// const name = superheroes.random();
+
+var name = randomSuperhero();
+// console.log(`My name is ${sillyName}.`);
+
+// console.log(superheroes);
+console.log(`I am ${name}!`);
