bf5056dc6aa199b58798fd079b8869b57e17cf00
Janice Nguyen <haidn@uci.edu>
2024-09-10 17:12:49 +0700

    API authorization

--- API/5.4 API Authentication/index.js
+++ API/5.4 API Authentication/index.js
@@ -2,30 +2,40 @@ import express from "express";
 import axios from "axios";
 
 const app = express();
-const port = 3000;
+const port = 4000;
 const API_URL = "https://secrets-api.appbrewery.com/";
 
 //TODO 1: Fill in your values for the 3 types of auth.
-const yourUsername = "";
-const yourPassword = "";
-const yourAPIKey = "";
-const yourBearerToken = "";
+const yourUsername = "janicenguyen03";
+const yourPassword = "duyennguyen";
+const yourAPIKey = "aa9ce665-56c6-4549-84af-90430302f461";
+const yourBearerToken = "23c348b3-637b-4601-8d82-778a28660c89";
 
 app.get("/", (req, res) => {
   res.render("index.ejs", { content: "API Response." });
 });
 
-app.get("/noAuth", (req, res) => {
+app.get("/noAuth", async (req, res) => {
   //TODO 2: Use axios to hit up the /random endpoint
-  //The data you get back should be sent to the ejs file as "content"
-  //Hint: make sure you use JSON.stringify to turn the JS object from axios into a string.
+  //  The data you get back should be sent to the ejs file as "content"
+  //  Hint: make sure you use JSON.stringify to turn the JS object from axios into a string.
+  try {
+    const response = await axios.get(API_URL + "random");
+    const result = JSON.stringify(response.data);
+    res.render("index.ejs", { content: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
 });
 
-app.get("/basicAuth", (req, res) => {
+app.get("/basicAuth", async (req, res) => {
   //TODO 3: Write your code here to hit up the /all endpoint
-  //Specify that you only want the secrets from page 2
-  //HINT: This is how you can use axios to do basic auth:
-  // https://stackoverflow.com/a/74632908
+  //  Specify that you only want the secrets from page 2
+  //  HINT: This is how you can use axios to do basic auth:
+  //  https://stackoverflow.com/a/74632908
   /*
    axios.get(URL, {
       auth: {
@@ -34,15 +44,44 @@ app.get("/basicAuth", (req, res) => {
       },
     });
   */
+ try {
+    const response = await axios.get(API_URL + "all?page=2", {
+                                      auth: {
+                                          username: yourUsername,
+                                          password: yourPassword
+                                      }, });
+    const result = JSON.stringify(response.data);
+    res.render("index.ejs", { content: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
 });
 
-app.get("/apiKey", (req, res) => {
+app.get("/apiKey", async (req, res) => {
   //TODO 4: Write your code here to hit up the /filter endpoint
   //Filter for all secrets with an embarassment score of 5 or greater
   //HINT: You need to provide a query parameter of apiKey in the request.
+  try {
+    const response = await axios.get(API_URL + "filter", {
+      params: {
+        score: 5,
+        apiKey: yourAPIKey,
+      },
+    });
+    const result = JSON.stringify(response.data);
+    res.render("index.ejs", { content: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
 });
 
-app.get("/bearerToken", (req, res) => {
+app.get("/bearerToken", async (req, res) => {
   //TODO 5: Write your code here to hit up the /secrets/{id} endpoint
   //and get the secret with id of 42
   //HINT: This is how you can use axios to do bearer token auth:
@@ -54,6 +93,20 @@ app.get("/bearerToken", (req, res) => {
     },
   });
   */
+  try {
+    const response = await axios.get(API_URL + "secrets/42", {
+      headers: { 
+        Authorization: `Bearer ${yourBearerToken}` 
+      },
+    });
+    const result = JSON.stringify(response.data);
+    res.render("index.ejs", { content: result });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error: error.message,
+    });
+  }
 });
 
 app.listen(port, () => {
