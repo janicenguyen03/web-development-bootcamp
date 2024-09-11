42ffbbb3450c7ac9240a0e443a29b8b796779275
Janice Nguyen <haidn@uci.edu>
2024-09-11 22:29:10 +0700

    REST API

--- API/5.5 REST APIs/solution.js
+++ API/5.5 REST APIs/solution.js
@@ -7,7 +7,7 @@ const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com";
 
 //Add your own bearer token from the previous lesson.
-const yourBearerToken = "08f3026d-9c6c-4d88-a3b2-c579dc106247";
+const yourBearerToken = "23c348b3-637b-4601-8d82-778a28660c89";
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
 };
